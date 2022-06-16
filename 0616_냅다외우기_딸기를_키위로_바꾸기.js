14f4499df4472239b3b71825385b93b21c5f1e8a
Moon Hee <sjmh0507@naver.com>
2022-06-16 20:07:53 +0900

    docs: Add a file

---
+++ 0616_냅다외우기_딸기를_키위로_바꾸기.js
@@ -0,0 +1,40 @@
+// 퀴즈1: 주어진 배열 안의 딸기 아이템을 키위로 교체하는 함수를 만들기
+// 단, 주어진 배열을 수정하지 않도록!
+// input: ['🍌', '🍓', '🍇', '🍓']
+// output: [ '🍌', '🥝', '🍇', '🥝' ]
+
+//+추가문제 '무슨 과일을' , '무슨 과일로' 바꾸고싶은지도 인풋값으로 입력해서 출력할 수 있게 해보기
+
+const array = ['🍌', '🍓', '🍇', '🍓'];
+
+// function replace(array) {
+//     let newReplace = Array.from(array);
+//     for (let i = 0; i < newReplace.length; i++) {
+//         if (newReplace[i] === '🍓') {
+//             newReplace[i] = '🥝'
+//         }
+//     }
+
+//     return newReplace;
+// }
+
+// const result = replace(array);
+// console.log(result);
+
+
+
+// 다른 과일도 바꾸고 싶다! 유연함 함수 만들기
+
+function replace(array, from, to) {
+    let newReplace = Array.from(array);
+    for (let i = 0; i < newReplace.length; i++) {
+        if (newReplace[i] === from) {
+            newReplace[i] = to;
+        }
+    }
+
+    return newReplace;
+}
+
+const result = replace(array, '🍌', '🍇');
+console.log(result);
